faf12ecd9568ad3c73898abf62b9ac18b6f40992
C1298297 Rafael Almeida <rafaelffa1@hotmail.com>
2020-02-04 17:27:40 -0300

    alteracao package

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(require('./app/router'));
 
-var port = process.env.port || 8001
-var port2 = process.env.port || 8002
+var port = process.env.PORT || 8080
+var port2 = process.env.PORT || 8002
 io.listen(port2);
 server.listen(port);
 console.log("Iniciando a app na porta " + port);
